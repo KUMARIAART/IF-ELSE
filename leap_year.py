60b0c9ec81c693ae5d626196ac22a9035bf509f8
AARTI KUMARI <89918154+KUMARIAART@users.noreply.github.com>
2022-02-12 11:35:12 +0530

    Update leap_year.py

--- leap_year.py
+++ leap_year.py
@@ -1,6 +1,12 @@
 year=int(input("enter the year:"))
-if year%4==0:
-    if year%400==0 and year%100==0:
-        print("leap year",year)
+if year%100==0:
+    if year%400==0:
+        print("leap year ",year)
+    else:
+        print("not a leap year")
+elif year%4==0:
+    print("leap year",year)
 else:
-    print("not leap year",year)
+    print("not a leap year")
+
+
